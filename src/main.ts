31c7a2d340e13e58df6fc49466a0f79df95eb40f
tanxin <txloveu@163.com>
2021-10-29 11:57:56 +0800

    chore: 去除service worker

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import Antd from 'ant-design-vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import '@/style/index.scss'
